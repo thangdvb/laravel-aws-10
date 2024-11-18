d920b7398ec18d3435e82d395ebdc14b62000816
Dao Van Bao Thang <thangdvb.nta@gmail.com>
2024-11-18 15:20:28 +0700

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
